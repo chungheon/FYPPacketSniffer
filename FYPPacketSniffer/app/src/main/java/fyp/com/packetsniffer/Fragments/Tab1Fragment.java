25e6d8b119b555ba1d9849c7329a5dcdcfd79f91
chungheon <36347616+chungheon@users.noreply.github.com>
2019-09-24 16:30:06 +0800

    Removed PacketCapture

--- FYPPacketSniffer/app/src/main/java/fyp/com/packetsniffer/Fragments/Tab1Fragment.java
+++ FYPPacketSniffer/app/src/main/java/fyp/com/packetsniffer/Fragments/Tab1Fragment.java
@@ -1,106 +1,21 @@
 package fyp.com.packetsniffer.Fragments;
 
-import android.content.BroadcastReceiver;
-import android.content.Context;
-import android.content.Intent;
-import android.content.IntentFilter;
-import android.net.VpnService;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v4.content.LocalBroadcastManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.TextView;
-
-import fyp.com.packetsniffer.PacketCapture.LocalVPNService;
 import fyp.com.packetsniffer.R;
-
-import static android.app.Activity.RESULT_OK;
 
 public class Tab1Fragment extends Fragment {
 
     private static final int VPN_REQUEST_CODE = 0x0F;
 
-    private boolean waitingForVPNStart;
-
     private View view;
-
-    private Button vpnButton;
-
-    private TextView output;
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragment_one, container, false);
-        vpnButton = (Button)view.findViewById(R.id.vpn);
-        output = (TextView) view.findViewById(R.id.outputText);
-        vpnButton.setOnClickListener(new View.OnClickListener()
-        {
-            @Override
-            public void onClick(View v)
-            {
-                startVPN();
-            }
-        });
-        waitingForVPNStart = false;
-        LocalBroadcastManager.getInstance(getContext()).registerReceiver(vpnStateReceiver,
-                new IntentFilter(LocalVPNService.BROADCAST_VPN_STATE));
         return view;
     }
 
-    private BroadcastReceiver vpnStateReceiver = new BroadcastReceiver()
-    {
-        public void onReceive(Context context, Intent intent)
-        {
-            if (LocalVPNService.BROADCAST_VPN_STATE.equals(intent.getAction()))
-            {
-                if (intent.getBooleanExtra("running", false))
-                    waitingForVPNStart = false;
-            }
-        }
-    };
-
-    private void startVPN()
-    {
-        Intent vpnIntent = VpnService.prepare(this.getContext());
-        if (vpnIntent != null)
-            startActivityForResult(vpnIntent, VPN_REQUEST_CODE);
-        else
-            onActivityResult(VPN_REQUEST_CODE, RESULT_OK, null);
-    }
-
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data)
-    {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == VPN_REQUEST_CODE && resultCode == RESULT_OK)
-        {
-            waitingForVPNStart = true;
-            getActivity().startService(new Intent(getActivity(), LocalVPNService.class));
-            enableButton(false);
-        }
-    }
-
-    @Override
-    public void onResume() {
-        super.onResume();
-
-        enableButton(!waitingForVPNStart && !LocalVPNService.isRunning());
-    }
-
-    private void enableButton(boolean enable)
-    {
-        final Button vpnButton = (Button) view.findViewById(R.id.vpn);
-        if (enable)
-        {
-            vpnButton.setEnabled(true);
-            vpnButton.setText("Start VPN");
-        }
-        else
-        {
-            vpnButton.setEnabled(false);
-            vpnButton.setText("Stop VPN");
-        }
-    }
 }
