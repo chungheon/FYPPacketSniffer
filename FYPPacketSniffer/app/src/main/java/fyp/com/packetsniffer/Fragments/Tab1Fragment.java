017bbb8cb4e42569b420cd7cca6f9e0f0ae77118
chungheon <36347616+chungheon@users.noreply.github.com>
2019-10-04 12:23:53 +0800

    First Commit
    Navigation Drawer Tutorial Example

--- FYPPacketSniffer/app/src/main/java/fyp/com/packetsniffer/Fragments/Tab1Fragment.java
+++ FYPPacketSniffer/app/src/main/java/fyp/com/packetsniffer/Fragments/Tab1Fragment.java
@@ -5,11 +5,19 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
 import fyp.com.packetsniffer.R;
 
 public class Tab1Fragment extends Fragment {
+    int num = 0;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_one, container, false);
+        View view = inflater.inflate(R.layout.fragment_one, container, false);
+        TextView tx = view.findViewById(R.id.fragText);
+        num = this.getArguments().getInt("num");
+        tx.setText(num + " fragment");
+        return view;
     }
 }
--- FYPPacketSniffer/app/src/main/java/fyp/com/packetsniffer/MainActivity.java
+++ FYPPacketSniffer/app/src/main/java/fyp/com/packetsniffer/MainActivity.java
@@ -1,23 +1,58 @@
 package fyp.com.packetsniffer;
 
+import android.drm.DrmStore;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
 import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
+import android.view.MenuItem;
 import android.view.View;
 
 import fyp.com.packetsniffer.Fragments.Tab1Fragment;
 import fyp.com.packetsniffer.Fragments.TabAdapter;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     private TabAdapter adapter;
     private TabLayout tabLayout;
     private ViewPager viewPager;
+    private DrawerLayout drawerLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        viewPager = (ViewPager) findViewById(R.id.viewPager);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar,
+                R.string.nav_drawer_open, R.string.nav_drawer_close);
+
+        NavigationView nv = findViewById(R.id.nav_view);
+        nv.setNavigationItemSelectedListener(this);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        if(savedInstanceState == null){
+            Tab1Fragment fragment1 = new Tab1Fragment();
+            Bundle args = new Bundle();
+            args.putInt("num", 1);
+            fragment1.setArguments(args);
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, fragment1).commit();
+            nv.setCheckedItem(R.id.test1);
+        }
+
+
+        /*viewPager = (ViewPager) findViewById(R.id.viewPager);
         tabLayout = (TabLayout) findViewById(R.id.tabLayout);
         adapter = new TabAdapter(getSupportFragmentManager());
         adapter.addFragment(new Tab1Fragment(), "Tab 1");
@@ -28,6 +63,44 @@ public class MainActivity extends AppCompatActivity {
         tabLayout.getTabAt(1).setIcon(R.drawable.packet_capture);
         View view1 = getLayoutInflater().inflate(R.layout.tab_one, null);
         view1.findViewById(R.id.icon).setBackgroundResource(R.drawable.packet_capture);
-        tabLayout.getTabAt(0).setCustomView(view1);
+        tabLayout.getTabAt(0).setCustomView(view1);*/
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        Tab1Fragment fragment1 = new Tab1Fragment();
+        Bundle args = new Bundle();
+        args.putInt("num", 1);
+        fragment1.setArguments(args);
+        Tab1Fragment fragment2 = new Tab1Fragment();
+        args = new Bundle();
+        args.putInt("num", 2);
+        fragment2.setArguments(args);
+        Tab1Fragment fragment3 = new Tab1Fragment();
+        args = new Bundle();
+        args.putInt("num", 3);
+        fragment3.setArguments(args);
+        switch(menuItem.getItemId()){
+            case R.id.test1: getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, fragment1).commit();
+                            break;
+            case R.id.test2: getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, fragment2).commit();
+                            break;
+            case R.id.test3: getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, fragment3).commit();
+                            break;
+
+        }
+
+        drawerLayout.closeDrawer(Gravity.START);
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }else{
+            super.onBackPressed();
+        }
+
     }
 }
